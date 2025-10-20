be45e81a9ee511159dab4f0f16cfbfcc80c1baa9
euriel-calimlim <eurielv4@gmail.com>
2025-10-20 16:18:04 +0400

    Add files via upload

---
+++ 1_exercise.py
@@ -0,0 +1,17 @@
+"""
+Exercise 1 : CODING IS COOL
+
+
+"""
+
+word1="Coding"
+word2="is"
+word3="Cool"
+#naming and assigning each word as individual variables.
+
+print(word1+word2+word3)
+#print function displays the output to user.
+#the '+' function adds/combines the variables together.
+
+
+
